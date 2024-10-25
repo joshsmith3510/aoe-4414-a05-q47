9b3cce6afdf2284aca3ca156ed4b10d8b528b93b
joshsmith3510 <144481161+joshsmith3510@users.noreply.github.com>
2024-10-24 21:38:06 -0400

    Add files via upload

---
+++ conv_ops.py
@@ -0,0 +1,72 @@
+# conv_ops.py
+#
+# Usage: python3 script_name.py arg1 arg2 ...
+#  Text explaining script usage
+# Parameters:
+#  arg1: description of argument 1
+#  arg2: description of argument 2
+#  ...
+# Output:
+#  A description of the script output
+#
+# Written by Brad Denby
+# Other contributors: Josh Smith
+#
+# Optional license statement, e.g., See the LICENSE file for the license.
+
+# import Python modules
+# e.g., import math # math module
+import sys # argv
+import math
+
+# "constants"
+# e.g., R_E_KM = 6378.137
+
+# helper functions
+
+## function description
+# def calc_something(param1, param2):
+#   pass
+
+# initialize script arguments
+c_in = float('nan')
+h_in = float('nan')
+w_in = float('nan')
+n_filt = float('nan')
+h_filt = float('nan')
+w_filt = float('nan')
+s = float('nan')
+p = float('nan')
+
+# parse script arguments
+if len(sys.argv)==9:
+    c_in = float(sys.argv[1])
+    h_in = float(sys.argv[2])
+    w_in = float(sys.argv[3])
+    n_filt = float(sys.argv[4])
+    h_filt = float(sys.argv[5])
+    w_filt = float(sys.argv[6])
+    s = float(sys.argv[7])
+    p = float(sys.argv[8])
+    ...
+else:
+    print(\
+        'Usage: '\
+            'python3 c_in h_in w_in n_filt h_filt w_filt s p ...'\
+                )
+    exit()
+
+# write script below this line
+h_out=((h_in+2*p-h_filt)/s)+1
+w_out=((w_in+2*p-w_filt)/s)+1
+adds=n_filt*h_out*w_out*c_in*h_filt*w_filt
+muls=adds
+c_out=c_in
+divs=0
+
+print(int(c_out)) # output channel count
+print(int(h_out)) # output height count
+print(int(w_out)) # output width count
+print(int(adds))  # number of additions performed
+print(int(muls))  # number of multiplications performed
+print(int(divs))  # number of divisions performed
